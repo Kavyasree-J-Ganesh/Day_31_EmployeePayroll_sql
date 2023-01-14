4d08ba3e7492041aab734e5bb226a2db95cba68b
Kavyasree-J-Ganesh <kavyasreejganesh97@gmail.com>
2023-01-14 23:24:25 +0530

    [Kavyasree j ganesh] addressbook sql

---
+++ src/Employee_payroll.sql
@@ -0,0 +1,16 @@
+---Use Case 1---
+---Ability to create a Address Book Service DB---
+
+mysql>create database payroll_service;
+1 row(s) affected
+
+mysql>show databases
+/*
+addressbook_service
+information_schema
+mysql
+payroll_service
+performance_schema
+sakila
+sys
+world */
